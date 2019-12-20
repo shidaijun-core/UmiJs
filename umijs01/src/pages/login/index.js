300de65138b0b262dff2d55c86f3be2c815cf875
sdj <18409319095@163.com>
2019-12-20 18:33:03 +0800

    day0202

--- umijs01/src/pages/login/index.js
+++ umijs01/src/pages/login/index.js
@@ -1,9 +1,11 @@
 
 import styles from './index.css';
+import router from '@/pages/.umi/router';
 export default function() {
   return (
     <div className={styles.normal}>
       <h1>Login</h1>
+      <button onClick={()=>{router.goBack()}}>go back</button>
     </div>
 
   );
--- umijs01/src/pages/test/index.js
+++ umijs01/src/pages/test/index.js
@@ -4,6 +4,7 @@ export default function() {
   return (
     <div className={styles.normal}>
       <h1>Login</h1>
+      <button to={"/index"}>go back</button>
     </div>
   );
 }
